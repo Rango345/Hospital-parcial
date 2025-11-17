22c820d64aefec3652caf6a9cd2d8413ad0e2014
juanc <juanc@LAPTOP-8KJ6942L>
2025-11-17 14:59:00 -0500

    Se creo la clase en modelo que contiene los atributos del hitorial medico

---
+++ src/main/java/modelo/Historial.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author juanc
+ */
+public class Historial {
+
+    private String enfermedadesPrevias;
+    private String cirugiasPrevias;
+    private String alergias;
+    private String medicamentosActuales;
+
+    
+    private String antecedentesFamiliares;
+
+   
+    private String motivoConsulta;
+    private String diagnostico;
+    private String tratamiento;
+    private String observaciones;
+
+    
+    public Historial() {
+    }
+
+    public String getEnfermedadesPrevias() {
+        return enfermedadesPrevias;
+    }
+
+    public void setEnfermedadesPrevias(String enfermedadesPrevias) {
+        this.enfermedadesPrevias = enfermedadesPrevias;
+    }
+
+    public String getCirugiasPrevias() {
+        return cirugiasPrevias;
+    }
+
+    public void setCirugiasPrevias(String cirugiasPrevias) {
+        this.cirugiasPrevias = cirugiasPrevias;
+    }
+
+    public String getAlergias() {
+        return alergias;
+    }
+
+    public void setAlergias(String alergias) {
+        this.alergias = alergias;
+    }
+
+    public String getMedicamentosActuales() {
+        return medicamentosActuales;
+    }
+
+    public void setMedicamentosActuales(String medicamentosActuales) {
+        this.medicamentosActuales = medicamentosActuales;
+    }
+
+    public String getAntecedentesFamiliares() {
+        return antecedentesFamiliares;
+    }
+
+    public void setAntecedentesFamiliares(String antecedentesFamiliares) {
+        this.antecedentesFamiliares = antecedentesFamiliares;
+    }
+
+    public String getMotivoConsulta() {
+        return motivoConsulta;
+    }
+
+    public void setMotivoConsulta(String motivoConsulta) {
+        this.motivoConsulta = motivoConsulta;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public String getTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(String tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+    
+    
+
+    
+}
